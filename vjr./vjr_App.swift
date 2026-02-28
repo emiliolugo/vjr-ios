302bf348ea67e3d8c657df3deda1981f7cbcdbb5
lugie314 <emilio23lugo@gmail.com>
2026-02-28 18:39:57 -0500

    Initial Commit

---
+++ vjr./vjr_App.swift
@@ -0,0 +1,17 @@
+//
+//  vjr_App.swift
+//  vjr.
+//
+//  Created by Emilio Lugo on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct vjr_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vjr.Tests/vjr_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  vjr_Tests.swift
+//  vjr.Tests
+//
+//  Created by Emilio Lugo on 2/28/26.
+//
+
+import Testing
+
+struct vjr_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
